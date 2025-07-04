05a443d306be0d228c6b3f804d52061b3a1a1a3d
kohara.h <kohara.h.00@dse-corp.co.jp>
2025-07-04 09:28:54 +0900

    test/husky

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 import { dirname } from 'path'
-import { FlatCompat } from '@eslint/eslintrc'
 import { fileURLToPath } from 'url'
 
+import { FlatCompat } from '@eslint/eslintrc'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
