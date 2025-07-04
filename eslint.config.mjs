75e6a37d04585d5116307fb2b251c70158a1e6e1
kohara.h <kohara.h.00@dse-corp.co.jp>
2025-07-04 09:23:44 +0900

    test

---
+++ eslint.config.mjs
@@ -0,0 +1,42 @@
+import { dirname } from 'path'
+import { FlatCompat } from '@eslint/eslintrc'
+import { fileURLToPath } from 'url'
+
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+})
+
+const eslintConfig = [
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    plugins: {
+      import: (await import('eslint-plugin-import')).default,
+    },
+    rules: {
+      'import/order': [
+        'error',
+        {
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            'parent',
+            'sibling',
+            'index',
+          ],
+          'newlines-between': 'always',
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+        },
+      ],
+    },
+  },
+]
+
+export default eslintConfig
